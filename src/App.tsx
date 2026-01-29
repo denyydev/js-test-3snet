b5f578759ae706668e0f076754496ae780655366
denyydev <polegenkydenis@gmail.com>
2026-01-29 20:38:15 +0300

    chore: init React + TypeScript + Tailwind project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-700">
+      <h1 className="text-3xl font-bold">JS Test – 3SNET</h1>
+    </div>
+  )
+}
+
+export default App
